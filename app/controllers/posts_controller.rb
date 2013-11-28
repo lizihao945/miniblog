e6299edb67ae87f34b0f69c6b25d66d1058d50f9
James Lee <lizihao945@gmail.com>
2013-11-28 16:42:47 +0800

    only owner can destory post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -81,11 +81,15 @@ class PostsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @post = Post.find(params[:id])
-    @post.destroy
-
     respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
+      if current_user == @post.user
+        @post.destroy
+        format.html { redirect_to posts_url }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to posts_url, notice: 'You can only destroy your own posts!' }
+        format.json { head :no_content }
+      end
     end
   end
 end
