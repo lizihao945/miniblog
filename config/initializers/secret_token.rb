02491df8b11c53accf5759582a80746bce67904e
James Lee <lizihao945@gmail.com>
2013-11-29 15:56:22 +0800

    use the secret_token.rb maybe works

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miniblog::Application.config.secret_token = 'e59fce6315ebf53dfa1d44f41208b92f0200b71bf8ce3b75e33111d2cc5e45c9f760bd80d393a2eddb7eb623ab1bca36a3aa3e4884afd68569b6847b152bf6ab'
